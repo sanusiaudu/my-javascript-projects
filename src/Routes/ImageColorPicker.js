c543a5ae58f11c0c091bdefc970d8f605784a776
sanusiaudu <saniaudu123@gmail.com>
2025-06-22 15:26:07 +0100

    fix/ build

--- src/Routes/ImageColorPicker.js
+++ src/Routes/ImageColorPicker.js
@@ -2,19 +2,18 @@
 import { useState } from "react";
 import { PiImageSquareLight } from "react-icons/pi";
 
-
 const ColorPicker = () => {
   const [color, setColor] = useState("#5524e7");
   const [image, setImage] = useState(null);
 
-    const openEyeDropper = async () => {
-        let eyeDropper = new EyeDropper();
-//     if ('EyeDropper' in window) {
-//   const eyeDropper = new EyeDropper();
-//   // Your logic here
-// } else {
-//   console.error("EyeDropper API is not supported in this browser.");
-// }
+  const openEyeDropper = async () => {
+    let eyeDropper = new EyeDropper();
+    //     if ('EyeDropper' in window) {
+    //   const eyeDropper = new EyeDropper();
+    //   // Your logic here
+    // } else {
+    //   console.error("EyeDropper API is not supported in this browser.");
+    // }
     const { sRGBHex } = await eyeDropper.open();
     setColor(sRGBHex);
   };
@@ -30,23 +29,22 @@ const ColorPicker = () => {
 
   return (
     <div className="grid grid-cols-[450px_1fr] min-h-screen text-white ">
-      <div className='w-full h-screen bg-[#22232b] p-6 top-0 sticky'>
+      <div className="w-full h-screen bg-[#22232b] p-6 top-0 sticky">
         <h1 className=" m-4">Pick color from image</h1>
 
         <div className="m-4">
-          <p className="text-xs m-1 text-[#ededf1]" >Select an image</p>
-                  <input
-                      onChange={handleFileInput}
-                      type="file" accept="image/*" />
+          <p className="text-xs m-1 text-[#ededf1]">Select an image</p>
+          <input onChange={handleFileInput} type="file" accept="image/*" />
         </div>
 
         <div className="mb-[1.6rem]">
           <p className="text-xs m-4 text-[#ededf1]">Pick color</p>
-                  <button className="w-full bg-[#6c738b] text-white border-0 text-base p-3 rounded cursor-pointer transition-colors duration-200  hover:bg-[#46495e]"
-                      onClick={openEyeDropper}
-                  >
+          <button
+            className="w-full bg-[#6c738b] text-white border-0 text-base p-3 rounded cursor-pointer transition-colors duration-200  hover:bg-[#46495e]"
+            onClick={openEyeDropper}
+          >
             Open Eyedropper
-          </button> 
+          </button>
         </div>
 
         <div className="m-4">
@@ -59,14 +57,18 @@ const ColorPicker = () => {
             <span>{color}</span>
           </button>
         </div>
-
       </div>
 
       <div className="bg-[#363844] p-10 grid place-items-center relative overflow-hidden">
         {image ? (
           <>
-            <img className="w-full h-auto max-w-4xl shadow-[0px 5px 15px rgba(0, 0, 0, 0.15)] relative z-10" src={image} alt="Working image" />
-            <div className="absolute inset-0 z-0 bg-cover bg-center scale-125 filter blur-sm opacity-50"
+            <img
+              className="w-full h-auto max-w-4xl shadow-[0px 5px 15px rgba(0, 0, 0, 0.15)] relative z-10"
+              src={image}
+              alt="Working"
+            />
+            <div
+              className="absolute inset-0 z-0 bg-cover bg-center scale-125 filter blur-sm opacity-50"
               style={{
                 backgroundImage: `url(${image})`,
               }}
